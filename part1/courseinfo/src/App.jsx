2487d396b19c022230fc61d6be0020495796a126
Juan Urdaneta <jurdanetac10@gmail.com>
2023-12-22 15:17:48 -0400

    `part1`: 1.3 - course information step3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -38,26 +38,30 @@ const Total = (props) => {
 }
 
 const App = () => {
-  // Main app component
-
-  // Define the course name and the parts and their exercise count
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const parts = [part1, part2, part3]
-  const exercises = [exercises1, exercises2, exercises3]
+  const names = parts.map(part => part.name)
+  const exerciseCounts = parts.map(part => part.exercises)
 
   return (
-    <>
+    <div>
       <Header course={course} />
-      <Content names={parts} exerciseCount={exercises}/>
-      <Total total={exercises.reduce((a, b) => a + b, 0)} />
-    </>
+      <Content names={names} exerciseCount={exerciseCounts} />
+      <Total total={exerciseCounts.reduce((a, b) => a + b, 0)} />
+    </div>
   );
 }
 
