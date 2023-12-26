7b9cfa802b9b029ff95bad943a69e7f8681c0fc3
Juan Urdaneta <jurdanetac10@gmail.com>
2023-12-25 20:34:04 -0400

    `part2`: 2.2 - course information step7

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -52,10 +52,17 @@ const Content = ( { parts } ) => {
 }
 
 const Course = ( {course} ) => {
+  // sum of total exercises for the course
+  const totalExercises = course.parts.reduce(
+    (accumulator, currentPart) => accumulator + currentPart.exercises, // sum
+    0 // initial value for the accumulator
+  );
+
   return (
     <>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <p><b>total of {totalExercises} exercises</b></p>
     </>
   );
 }
@@ -79,6 +86,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
