4d8adfb885a8690a77cd3a33c43ccbb63b81d74f
Juan Urdaneta <jurdanetac10@gmail.com>
2024-02-05 15:27:48 -0400

    `part6`: 6.9 Better anecdotes, step7

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,13 @@ import { useSelector, useDispatch } from "react-redux";
 import { vote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const state = useSelector((state) => state);
+
+  const filter = state.filter;
+  const anecdotes = state.anecdotes.filter((anecdote) =>
+    anecdote.content.trim().toLowerCase().includes(filter.trim().toLowerCase()),
+  );
+
   const dispatch = useDispatch();
 
   return (
