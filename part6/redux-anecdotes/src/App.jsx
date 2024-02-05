c8ac88476e5fb7d5eae4bb67d809b1eb05ef1b1d
Juan Urdaneta <jurdanetac10@gmail.com>
2024-02-04 20:25:24 -0400

    `part6`: 6.7: anecdotes, step5

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,22 +1,10 @@
-import { vote, createAnecdote } from './reducers/anecdoteReducer'
+import { vote } from './reducers/anecdoteReducer'
 import { useSelector, useDispatch } from 'react-redux'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-
-    // get the content of the input field
-    const content = event.target.anecdote.value
-
-    // flush the input field
-    event.target.anecdote.value = ''
-
-    // dispatch the action
-    dispatch(createAnecdote(content))
-  }
 
   return (
     <div>
@@ -33,11 +21,7 @@ const App = () => {
             <button onClick={() => dispatch(vote(anecdote))}>vote</button>
           </div>
         </div>)}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="anecdote" /></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   )
 }
---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,31 @@
+import { createAnecdote } from '../reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+
+    // get the content of the input field
+    const content = event.target.anecdote.value
+
+    // flush the input field
+    event.target.anecdote.value = ''
+
+    // dispatch the action
+    dispatch(createAnecdote(content))
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input name="anecdote" /></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+}
+
+export default AnecdoteForm
