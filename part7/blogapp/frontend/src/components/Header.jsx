922d02c4514c2779c166705f2bddd25e546e2046
Juan Urdaneta <jurdanetac10@gmail.com>
2024-02-19 14:25:28 -0400

    `part7`: 7.17: Navigation

--- part7/blogapp/frontend/src/components/Header.jsx
+++ part7/blogapp/frontend/src/components/Header.jsx
@@ -1,19 +1,40 @@
+import { Link } from "react-router-dom";
 import Notification from "./Notification";
 
 const Header = ({ user, message, type, handleLogout }) => {
   // if logged in, show user's name and logout button
   if (user) {
+    const padding = {
+      padding: 5,
+    };
+
+    const background = {
+      backgroundColor: "grey",
+      padding: 5,
+    };
+
     return (
       <div>
-        <h2>blogs</h2>
-        <Notification message={message} type={type} />
+        <div style={background}>
+          <Link style={padding} to="/">
+            home
+          </Link>
+          <Link style={padding} to="/blogs">
+            blogs
+          </Link>
+          <Link style={padding} to="/users">
+            users
+          </Link>
+          {user !== null && user.name ? (
+            <>
+              {user.name} logged in{" "}
+              <button onClick={handleLogout}>logout</button>
+            </>
+          ) : null}
+        </div>
 
-        {user !== null && user.name ? (
-          <p>
-            {user.name} logged in <br />{" "}
-            <button onClick={handleLogout}>logout</button>
-          </p>
-        ) : null}
+        <h2>blog app</h2>
+        <Notification message={message} type={type} />
       </div>
     );
   }
