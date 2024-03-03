4a2ed7a2c79ace4b38f80dc9ef1b539313a4286d
Juan Urdaneta <jurdanetac10@gmail.com>
2024-03-03 16:35:21 -0400

    `part8`: 8.18 Log in

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,11 +1,16 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, useApolloClient } from "@apollo/client";
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm";
 import { ALL_AUTHORS, ALL_BOOKS } from "./queries";
 
 const App = () => {
+  const [token, setToken] = useState(null);
+  const client = useApolloClient();
+
   const padding = {
     padding: 5,
   };
@@ -17,6 +22,12 @@ const App = () => {
     return <div>loading...</div>;
   }
 
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
   return (
     <Router>
       <div>
@@ -26,13 +37,25 @@ const App = () => {
         <Link style={padding} to="/books">
           books
         </Link>
-        <Link style={padding} to="/add">
-          add book
-        </Link>
+        {token ? (
+          <>
+            <Link style={padding} to="/add">
+              add book
+            </Link>
+            <Link style={padding} onClick={logout}>
+              logout
+            </Link>
+          </>
+        ) : (
+          <Link style={padding} to="/login">
+            login
+          </Link>
+        )}
       </div>
 
       <div>
         <Routes>
+          <Route path="/" element={<></>} />
           <Route
             path="/authors"
             element={<Authors authors={authors.data.allAuthors} />}
@@ -41,6 +64,7 @@ const App = () => {
             path="/books"
             element={<Books books={books.data.allBooks} />}
           />
+          <Route path="/login" element={<LoginForm setToken={setToken} />} />
           <Route path="/add" element={<NewBook />} />
         </Routes>
       </div>
