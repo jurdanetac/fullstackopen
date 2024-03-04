6aee4974e21d94687356573acdd544f006b6aa00
Juan Urdaneta <jurdanetac10@gmail.com>
2024-03-04 11:32:44 -0400

    `part8`: 8.19 Books by genre, part 1

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 const Books = ({ books }) => {
+  const [genre, setGenre] = useState("all genres");
+  const genres = books
+    .flatMap((b) => b.genres)
+    .filter((value, index, self) => self.indexOf(value) === index);
+  genres.push("all genres");
+
   return (
     <div>
       <h2>books</h2>
+      <p>
+        in genre <b>{genre}</b>
+      </p>
 
       <table>
         <tbody>
@@ -10,15 +21,22 @@ const Books = ({ books }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter((b) => b.genres.includes(genre) || genre === "all genres")
+            .map((a) => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      {genres.map((g) => (
+        <button key={g} onClick={() => setGenre(g)}>
+          {g}
+        </button>
+      ))}
     </div>
   );
 };
