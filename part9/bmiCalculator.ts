8a4d57a92e5bae4bb778db1129cc718508a08fdb
Juan Urdaneta <jurdanetac10@gmail.com>
2024-03-11 10:36:23 -0400

    `part9`: 9.1 Body mass index

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,28 @@
+// BMI = weight (kg) / (height (m) * height (m))
+
+const calculateBmi = (height: number, weight: number): string => {
+  const heightInMeters = height / 100;
+  const bmi = weight / (heightInMeters * heightInMeters);
+
+  // BMI Categories:
+  // https://en.wikipedia.org/wiki/Body_mass_index#Categories
+  if (bmi < 16) {
+    return "Underweight (Severe thinness)";
+  } else if (bmi <= 16.9) {
+    return "Underweight (Moderate thinness)";
+  } else if (bmi <= 18.4) {
+    return "Underweight (Mild thinness)";
+  } else if (bmi <= 24.9) {
+    return "Normal (healthy weight)";
+  } else if (bmi <= 29.9) {
+    return "Overweight (Pre-obese)";
+  } else if (bmi <= 34.9) {
+    return "Obese (Class I)";
+  } else if (bmi <= 39.9) {
+    return "Obese (Class II)";
+  } else {
+    return "Obese (Class III)";
+  }
+};
+
+console.log(calculateBmi(180, 74));
