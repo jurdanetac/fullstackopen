d55e6e871295507de4f0090807292028b9636cdb
Juan Urdaneta <jurdanetac10@gmail.com>
2024-03-15 11:27:06 -0400

    `part9`: 9.9: Patientor backend, step2

--- part9/patientor-backend/build/index.js
+++ part9/patientor-backend/build/index.js
@@ -1,16 +1,20 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors = require("cors");
 const app = (0, express_1.default)();
+app.use(cors());
 app.use(express_1.default.json());
-const PORT = 3000;
+const PORT = 3001;
 app.get("/api/ping", (_req, res) => {
-    console.log("someone pinged here");
-    res.send("pong");
+  console.log("someone pinged here");
+  res.send("pong");
 });
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
